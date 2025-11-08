4a7fd69e9f758303deb3f59a28a0b5d694892f84
Felix Tetteh <felixdev48@gmail.com>
2025-11-08 20:28:41 +0000

    Add test cases for printStatement function

--- 4-concat.js
+++ 4-concat.js
@@ -2,4 +2,9 @@ const printStatement = (arg1, arg2) => {
   console.log(`${arg1} is ${arg2}`);
 };
 
+console.log("--- Test Case 1 ---");
 printStatement("Banana", "Yellow");
+
+console.log("\n--- Test Case 2 ---");
+printStatement("The script", "completed successfully");
+
